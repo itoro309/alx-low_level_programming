f087c60580c9d6671e4f3e907af7c96859bad997
itoro309 <louis4live@gmail.com>
2022-12-17 19:46:48 -0800

    heed

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural
+ *
+ * @n: counting
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
