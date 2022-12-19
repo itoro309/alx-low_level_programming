3533184a75ed1c77affcf62eb1d6b99f0ed024fa
itoro309 <louis4live@gmail.com>
2022-12-19 08:54:03 -0800

    hie

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,26 @@
-#include "main.h"`
+#include "main.h"
+
+/**
+ * puts_half - function
+ * if odd len
+ * @str: input
+ * Return: half of half
+ */
+void puts_half(char *str)
+{
+	int i, n, len;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	n = (len / 2);
+
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
