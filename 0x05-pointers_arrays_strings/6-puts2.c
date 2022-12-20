381e437b11607f530de45957989dd6f7d60b3da8
itoro309 <louis4live@gmail.com>
2022-12-19 20:41:23 -0800

    meeed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- * rev_string - function
- * @s: input
- * Return: string
+ * puts2 - function
+ * startin
+ * @str: input
+ * Return: print
  */
-void rev_string(char *s)
+void puts2(char *str)
 {
-	char rev = s[0];
-	int fcounter = 0;
-	int i;
+	int i = 0;
 
-	while (s[fcounter] != '\0')
-		fcounter++;
-
-	for (i = 0; i < fcounter; i++)
+	for (; str[i] != '\0'; i++)
 	{
-		fcounter--;
-		rev = s[i];
-		s[i] = s[fcounter];
-		s[fcounter] = rev;
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
 	}
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,6 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void puts2(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -13,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+void puts2(char *str);
 
 #endif
