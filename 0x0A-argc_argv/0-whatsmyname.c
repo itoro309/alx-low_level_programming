9c864043afd3465013093a9899f69624cafe10ac
itoro309 <louis4live@gmail.com>
2023-01-06 13:04:55 -0800

    himm

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints
+ * @argc: Number of command
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
