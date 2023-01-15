1c41564103df4c7fdd02cbbf06f42f417514c22e
itoro309 <louis4live@gmail.com>
2023-01-14 21:50:08 -0800

    titt

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconnect - function
+ * @s1: destination
+ * @s2: source of string
+ * @n: int type for size
+ * Return: pointer to new
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+	int count, count1;
+	int sign = n;
+	char *ptr;
+	int len1, len2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (len1 = 0; s1[ln1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+	if (sign >= len2)
+	{
+		sign = len2;
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
+		if (ptr == NULL)
+			return (NULL);
+		for (count = 0; count < len1; count++)
+		{
+			ptr[count] = s1[count];
+		}
+		for (count1 = 0; count1 < sign; count1++)
+		{
+			ptr[count++] = s2[count1];
+		}
+		ptr[count++] = '\0';
+		return (ptr);
+}
